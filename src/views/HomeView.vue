3c651e269ea3073a0c799a613789ac577e69ca0b
X-bear <522168288@qq.com>
2022-12-16 10:26:23 +0800

    up css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mx-8 mt-4 HomeView">
-    <div class="mb-4 flex-grow">
+    <div class="mb-4 flex-1">
       <Progress class="mb-4"></Progress>
       <Action></Action>
     </div>
 
-    <Setting></Setting>
+    <Setting class="flex-1"></Setting>
   </div>
 </template>
 <script lang="ts" setup>
